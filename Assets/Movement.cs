9d8b8a33e59d9364b7670ec909a4ac91970e948b
s25597 <s25597@pjwstk.edu.pl>
2023-03-06 10:46:53 +0100

    Koniec zajęć

---
+++ Assets/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float upForce = 100;
+    public float speed = 1500;
+    public float runSpeed = 5000;
+
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    //ligma
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, rb.velocity.y);
+        }
+        else 
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) 
+        {
+            rb.AddForce(Vector2.up * upForce);
+            isGrounded = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
